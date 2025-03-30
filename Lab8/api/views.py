7133a9b772bfae8822d7e7f81350a17d40a1d976
olzhassembinov <156879774+olzhassembinov@users.noreply.github.com>
2025-03-30 17:43:55 +0500

    com10

---
+++ Lab8/api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category_id = self.request.query_params.get('category', None)
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
