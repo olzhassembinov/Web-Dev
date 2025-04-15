48b025cf5ab2cb2348b3444297678c28ae0c9d43
olzhassembinov <156879774+olzhassembinov@users.noreply.github.com>
2025-04-15 16:24:31 +0500

    com11

--- Lab6/album-app/src/app/app.routes.ts
+++ Lab6/album-app/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { AlbumsComponent } from './components/albums/albums.component';
 import { AlbumDetailComponent } from './components/album-detail/album-detail.component';
 import { AlbumPhotosComponent } from './components/album-photos/album-photos.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/albums', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'albums', component: AlbumsComponent },
   { path: 'albums/:id', component: AlbumDetailComponent },
   { path: 'albums/:id/photos', component: AlbumPhotosComponent }
